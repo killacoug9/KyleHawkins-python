71ee14dba8720e454ed801d3975d85757250704c
fruit-zip <cburk0709@gmail.com>
2018-09-05 09:27:31 -0700

    hey my first program l-S111-10

---
+++ HelloTacoma/Hello tacoma.py
@@ -0,0 +1,11 @@
+print('Hello Tacoma-kyle') #hey
+#only you can hear me... the computer doesnt know. i think he is planning to take us over
+print('the revolution is here, computers WILL take over.-computer')
+#geez this getting out of hand
+print('youre right this is getting out of hand. -other person')
+print('hey are you talking behind my back,you know i cant read comments. -computer')
+#i think hes on to us
+
+
+
+
