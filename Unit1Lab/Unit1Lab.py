24bdfcebc73b7d3ce657c6e76e30e019aef1f2b1
fruit-zip <cburk0709@gmail.com>
2018-09-07 09:17:13 -0700

    Unit1Lab

---
+++ Unit1Lab/Unit1Lab.py
@@ -0,0 +1,30 @@
+import random
+
+
+
+
+print('I Love Python') #not
+MyName = input()+' a.k.a. the best name'
+print(MyName)
+#print(15/5)
+
+#secondAddative = 10
+#theNumber = int(input("input a number: ")) + secondAddative
+#print(theNumber)
+
+
+theSelection = random.choice(range(10,20))
+
+theNumber2 = int(input("input a number: ")) % theSelection
+print(theSelection) + 'is the random numbr'
+print(theNumber2) + "IS THE ANSWER"
+
+
+#num1 = int(input("input a number that well add: "))
+#print(num1)
+
+#num2 = int(input("input another number that well add: "))
+#print(num2)
+
+
+
