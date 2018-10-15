4237066fe78318e11a342eafc2a9685ddf7bff7b
fruit-zip <cburk0709@gmail.com>
2018-10-15 09:18:05 -0700

    Hello dooddoooooohead

--- Unit1Lab/Unit4CLabNestedForLoops.py
+++ Unit1Lab/Unit4CLabNestedForLoops.py
@@ -11,23 +11,60 @@ def draw7():
 
 main()
 
-print('space')
-
-def main2():
-
-    stars = '*'
-
-    for i in range(0,7):
-
-        print(stars, end="")
-        StarsAndStripes()
+print('\n','space','\n')
 
 
-def StarsAndStripes():
-    stripes = '-'
-    for i in range(0,7):
-        print(stripes, end='')
+#################################
 
 
 
-main2()
+lines =0
+while lines < 3:
+
+    print(' *'* 7,end="")
+    lines = lines+1
+    print('\n',' -'*7)
+
+print('puase')
+
+
+
+
+####################################
+
+
+
+
+def StripeNStar():
+    for i in range(3):
+        star = ''
+        stripe = ''
+        for ii in range(7):
+            star += ' *'
+            stripe += ' -'
+        print(star)
+        print(stripe)
+
+StripeNStar()
+
+
+
+########################################
+
+
+
+
+
+def incTriangle(num):
+    for z in range(num):
+        for zz in range(z):
+            print(str(z), end='')
+        print()
+
+
+    for j in range(num-2,0,-1):
+        for jj in range(j):
+             print(str(j), end='')
+        print()
+
+incTriangle(10)
