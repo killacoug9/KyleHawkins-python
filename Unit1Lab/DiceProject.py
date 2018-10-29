a139b93999db1a06b74c86f51f69815ff42ef0e3
fruit-zip <cburk0709@gmail.com>
2018-10-29 09:19:48 -0700

    Hello dooddoooooohead

--- Unit1Lab/DiceProject.py
+++ Unit1Lab/DiceProject.py
@@ -4,17 +4,15 @@ from random import *
 def main():
     loop = "y"
     timesRun = 0
+
     while loop == "y":
+
         timesRun = timesRun + 1
         y = findRandNum()
         print(createDie(y))
         print('lucky dog you got a ' + str(y))
+        print('youve run it ' + str(timesRun) + ' times')
         loop = input('do u wanna keep going y or n')
-
-
-
-
-
 
 
 
@@ -28,13 +26,48 @@ def findRandNum():
 
 
 
+
+
 def createDie(findRandNum):
-    topandbottom = '-------'
+    topandbottom = '--------'
     noneStar = '|      |'
     oneStarCenter = '|  *   |'
     oneStarRight = '|    * |'
     twoStar = '| *  * |'
     oneStarLeft = '| *    |'
-    dice1 = [topandbottom, noneStar,oneStarCenter, noneStar, topandbottom]
-    print(dice1)
+    diceNumber1 = ['']
+    if findRandNum == 1:
+        diceNumber1 = [topandbottom, noneStar, oneStarCenter, noneStar, topandbottom]
+    elif findRandNum == 2:
+        diceNumber1 = [topandbottom,noneStar,twoStar,noneStar,topandbottom]
+    elif findRandNum == 3:
+        diceNumber1 = [topandbottom,oneStarLeft,oneStarCenter,oneStarRight, topandbottom]
+    elif findRandNum == 4:
+        diceNumber1 = [topandbottom, twoStar, noneStar, twoStar,topandbottom]
+    elif findRandNum == 5:
+        diceNumber1 = [topandbottom, twoStar, oneStarCenter, twoStar, topandbottom]
+    elif findRandNum == 6:
+        diceNumber1 = [ topandbottom, twoStar, twoStar, twoStar,topandbottom]
+
+    firstplace = diceNumber1
+
+    for LinesToPrint in firstplace:
+        print(LinesToPrint)
+
+
+
+    # one = [topandbottom, noneStar, oneStarCenter, noneStar, topandbottom]
+    # two = [topandbottom,noneStar,twoStar,noneStar,topandbottom]
+    # three = [topandbottom,oneStarLeft,oneStarCenter,oneStarRight, topandbottom]
+    # four = [topandbottom, twoStar, noneStar, twoStar,topandbottom]
+    # five = [topandbottom, twoStar, oneStarCenter, twoStar, topandbottom]
+    # six = [ topandbottom, twoStar, twoStar, twoStar,topandbottom]
+
+
+
+
+
+
+
+
 main()
--- Unit1Lab/holding commented out stuff for porject.py
+++ Unit1Lab/holding commented out stuff for porject.py
@@ -8,3 +8,15 @@
     #finishedMathGPA = uncalculatedGPA / num
      #   print(finishedMathGPA)##this is printing YOUR GPA THIS IS WHAT I WANT
 
+    if findRandNum == 1:
+        diceNumber1 = [topandbottom, noneStar, oneStarCenter, noneStar, topandbottom]
+    elif findRandNum == 2:
+        diceNumber1 = [topandbottom,noneStar,twoStar,noneStar,topandbottom]
+    elif findRandNum == 3:
+        diceNumber1 = [topandbottom,oneStarLeft,oneStarCenter,oneStarRight, topandbottom]
+    elif findRandNum == 4:
+        diceNumber1 = [topandbottom, twoStar, noneStar, twoStar,topandbottom]
+    elif findRandNum == 5:
+        diceNumber1 = [topandbottom, twoStar, oneStarCenter, twoStar, topandbottom]
+    elif findRandNum == 6:
+        diceNumber1 = [ topandbottom, twoStar, twoStar, twoStar,topandbottom]
