e6f565bd7cbb1a7ea49469ea1d9f28fd3b0cdff3
fruit-zip <cburk0709@gmail.com>
2018-11-02 09:20:28 -0700

    Hello dooddoooooohead

--- Unit1Lab/DiceProject.py
+++ Unit1Lab/DiceProject.py
@@ -4,31 +4,38 @@ from random import *
 def main():
     loop = "y"
     timesRun = 0
+    bigList = []
 
 
     while loop == "y":
 
         timesRun = timesRun + 1
-        y = findRandNum()
-        print(createDie(y))
-        print('lucky dog you got a ' + str(y))
+
+        Rnum1 = findRandNum()
+        diceList = createDie(Rnum1)
+        timesnum = input('how many die')
+
+        for z in range(0, int(timesnum)):
+            bigList.append(diceList[roll()])
+
+        makeDice(bigList)
+
+
+
+        print('lucky dog you got a ' + str(Rnum1))
         print('youve run it ' + str(timesRun) + ' times')
         loop = input('do u wanna keep going y or n')
 
 
-    diceList = createDie(findRandNum)
-    makeDice(diceList)
 
 
 
 
-def makeDice(diceList):
-    #print(diceList)
 
-    for i in range(0, len(diceList[0])):
 
-        for sublist in diceList:
-            print(sublist[0])
+
+
+
 
 
 
@@ -36,8 +43,10 @@ def makeDice(diceList):
 
 
 def findRandNum():
-    x = randint(1,6)
-    return x
+    Rnum1 = randint(1,6)
+
+    return Rnum1
+
 
 
 
@@ -45,51 +54,47 @@ def findRandNum():
 
 
 def createDie(findRandNum):
+
     topandbottom = '--------'
     noneStar = '|      |'
     oneStarCenter = '|  *   |'
     oneStarRight = '|    * |'
     twoStar = '| *  * |'
     oneStarLeft = '| *    |'
-    dice1 = ''
-    dice2 =''
-    dice3 =''
-    dice4=''
-    dice5=''
-    dice6=''
-    if findRandNum == 1:
-        dice1 = [topandbottom, noneStar, oneStarCenter, noneStar, topandbottom]
-    elif findRandNum == 2:
-        dice2 = [topandbottom,noneStar,twoStar,noneStar,topandbottom]
-    elif findRandNum == 3:
-        dice3 = [topandbottom,oneStarLeft,oneStarCenter,oneStarRight, topandbottom]
-    elif findRandNum == 4:
-        dice4 = [topandbottom, twoStar, noneStar, twoStar,topandbottom]
-    elif findRandNum == 5:
-        dice5 = [topandbottom, twoStar, oneStarCenter, twoStar, topandbottom]
-    elif findRandNum == 6:
-        dice6 = [ topandbottom, twoStar, twoStar, twoStar,topandbottom]
+    diceList = []
+
+    diceList.append([topandbottom, noneStar, oneStarCenter, noneStar, topandbottom])
+
+    diceList.append([topandbottom,noneStar,twoStar,noneStar,topandbottom])
+
+    diceList.append([topandbottom,oneStarLeft,oneStarCenter,oneStarRight, topandbottom])
+
+    diceList.append([topandbottom, twoStar, noneStar, twoStar,topandbottom])
+
+    diceList.append([topandbottom, twoStar, oneStarCenter, twoStar, topandbottom])
+
+    diceList.append([ topandbottom, twoStar, twoStar, twoStar,topandbottom])
 
 
-    dice1 = [topandbottom, noneStar, oneStarCenter, noneStar, topandbottom]
-    dice2 = [topandbottom,noneStar,twoStar,noneStar,topandbottom]
-    dice3 = [topandbottom,oneStarLeft,oneStarCenter,oneStarRight, topandbottom]
-    dice4 = [topandbottom, twoStar, noneStar, twoStar,topandbottom]
-    dice5 = [topandbottom, twoStar, oneStarCenter, twoStar, topandbottom]
-    dice6 = [ topandbottom, twoStar, twoStar, twoStar,topandbottom]
 
-
-    diceList = [dice1, dice2, dice3, dice4, dice5,dice6]
 
     return diceList
 
+def makeDice(diceList):
+    #print(diceList)
+
+    for row in range(0, len(diceList[0])):
+        for col in range(0,len(diceList)):
+            print (diceList[col][row], end='\t')
+        print()
 
 
 
 
+def roll():
+    randomnum1 = randint(0,6)
 
-
-
+    return randomnum1
 
 
 main()
