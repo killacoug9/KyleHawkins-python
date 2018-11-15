30d7f988f89ef938ce9186b37e86f4a4fdcc47f5
fruit-zip <cburk0709@gmail.com>
2018-11-15 09:18:55 -0800

    Hello dooddoooooohead

--- Unit1Lab/RealTProject.py
+++ Unit1Lab/RealTProject.py
@@ -1,19 +1,26 @@
 import turtle as t
 pen = t.Pen()
+t.bgcolor('grey')
 
 def main():
     line()
     square()
     circle()
-
+    triangle()
+    pen.clear()
+    adv()
 
 def line():
+    t.colormode(255)
+    pen.color(50,205,50)
     pen.up()
     pen.goto(-50,0)
     pen.down()
     pen.forward(100)
 
 def square():
+    t.colormode(hex)
+    pen.color('#0000ff')
     pen.up()
     pen.goto(-100,100)
     pen.down()
@@ -26,10 +33,42 @@ def square():
     pen.forward(200)
 
 def circle():
+
+    pen.color('red')
     pen.up()
     pen.goto(300,0)
     pen.down()
     pen.circle(300)
+
+def triangle():
+    pen.color
+    pen.up()
+    pen.goto(-200, -150)
+    pen.down()
+    pen.right(90)
+    pen.forward(400)
+    pen.left(120)
+    pen.forward(400)
+    pen.left(120)
+    pen.forward(400)
+
+
+def adv():
+    pen.up()
+    pen.goto(0,0)
+    pen.down()
+
+    sides = input('how many sides')
+    length = input('how long ')
+    pen.left(90)
+
+    angle = 180*(float(sides)-2)/float(sides)
+
+    for x in range(0, int(sides)):
+        pen.forward(float(length))
+        pen.right(float(angle))
+
+
 
 
 
@@ -37,3 +76,4 @@ def circle():
 
 main()
 t.exitonclick()
+t.update()
