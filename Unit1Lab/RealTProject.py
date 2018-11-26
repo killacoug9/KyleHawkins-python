56f549846b1736b3bbc3bedd3de13f0bb49c62d3
fruit-zip <cburk0709@gmail.com>
2018-11-26 09:13:39 -0800

    Hello dooddoooooohead

--- Unit1Lab/RealTProject.py
+++ Unit1Lab/RealTProject.py
@@ -83,9 +83,13 @@ def adv():
     # if colorType == 'random':
     #     colorUse = pen.color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
     #
-    sides = input('how many sides')
-    length = input('how long ')
+    # sides = input('how many sides')
+    # length = input('how long ')
     pen.left(90)
+
+    sides = 12
+    length=100
+
 
 
     if abs(int(sides)) < 3:
@@ -117,10 +121,96 @@ def space():
     pen.goto(0,0)
     pen.down()
 
+    pen.up()
+    pen.goto(-200,200)
+    pen.down()
+    pen.left(35)
+
+    #
+    # sides = input('how many sides')
+    # length = input('how long ')
+    sides=9
+    length=70
+    pen.left(90)
+
+
+    if abs(int(sides)) < 3:
+        print('wow i know what youre doing') #counter for too little sides
+        sides = random.randint(3, 20)
+
+    if abs(int(length)) > 100:
+        print('hey thats too long')
+        length = random.randint(50, 100) # counter for too long
+
+    if abs(int(sides)) > 20:
+        print('no more than 20 sides') #counter for too many
+        sides = random.randint(3, 20)
+
+
+    angle = 360/abs((float(sides)))
+
+    for x in range(0, abs(int(sides))):
+        #colorUse
+
+        pen.color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+        pen.forward(abs(float(length)))
+        pen.right(abs(float(angle)))
+####################################################################################
+#triangle top right
+    for x in range(0, 3):
+        pen.color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    pen.up()
+    pen.goto(200, 200)
+    pen.down()
+    pen.right(90)
+    pen.forward(200)
+    pen.left(120)
+    pen.forward(200)
+    pen.left(120)
+    pen.forward(200)
 
 
 
 
+
+
+
+
+
+###############
+#circle, bottom left
+    for x in range(0, 2):
+        pen.color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+
+    pen.up()
+    pen.goto(-300, -220)
+    pen.down()
+    pen.circle(100)
+########################################
+#square
+    for x in range(0,4):
+        pen.color(random.randint(0,255), random.randint(0,255), random.randint(0,255))
+    pen.up()
+    pen.goto(270, -275)
+    pen.down()
+
+    pen.right(65)
+    pen.forward(200)
+    pen.right(90)
+    pen.forward(200)
+    pen.right(90)
+    pen.forward(200)
+    pen.right(90)
+    pen.forward(200)
+
+
+
+
+
+
+
+
+##############################
 
 
 
