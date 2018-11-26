190b8302cd21f802915435f935e028a6c0967e2f
fruit-zip <cburk0709@gmail.com>
2018-11-26 09:18:51 -0800

    hola doodoo head

---
+++ Unit1Lab/Unit6ALab.py
@@ -0,0 +1,20 @@
+def main():
+    textStuff()
+
+
+
+
+def textStuff():
+   textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Britians Repealing the Border'}
+
+
+
+
+
+
+
+
+
+
+
+main()
