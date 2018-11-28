13d5d0bdd8e5b16b5fa01624b556b5e4cfbb2493
fruit-zip <cburk0709@gmail.com>
2018-11-28 10:10:23 -0800

    hola doodoo head

--- Unit1Lab/Unit6ALab.py
+++ Unit1Lab/Unit6ALab.py
@@ -1,11 +1,61 @@
+import random
+#http://openbookproject.net/thinkcs/python/english3e/dictionaries.html# about directories
+
+
 def main():
     textStuff()
 
 
+#
+# def searchSystem(searchCode):
+#
+#
 
 
 def textStuff():
-   textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Britians Repealing the Border'}
+    KeepGoing = 'yes'
+    while KeepGoing == 'yes':
+        searchCode = input('what abbreviation you want')
+        textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Better Redistribute Bread','LOL':'Lenin our leader', 'OMG':'Opponents meet gulag','LMAO':'Leninist-Marxist allied operation'}
+
+
+        if searchCode in textCodes:
+            print(textCodes[searchCode])
+        else:
+            WantToMakeInput = input('would u like to make new option')
+
+        if WantToMakeInput == 'yes':
+            print('ok')
+        elif WantToMakeInput == 'no':
+            print('thats too bad')
+
+        KeepGoing = input('wanna find another abreviation')
+
+
+
+
+
+
+    #
+    # searchCode = input('what abbreviation you want')
+    # textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Better Redistribute Bread','LOL':'Lenin our leader', 'OMG':'Opponents meet gulag','LMAO':'Leninist-Marxist allied operation'}
+    #
+    #
+
+
+    #
+    # if searchCode in textCodes:
+    #      print(textCodes[searchCode])
+    # else:
+    #     WantToMakeInput = input('would u like to make new option')
+    #
+    # if WantToMakeInput == 'yes':
+    #     print('ok')
+    #
+
+
+
+
 
 
 
