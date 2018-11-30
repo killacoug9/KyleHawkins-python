4534db35cc31769aa1d12a9d8acba750171e60a3
fruit-zip <cburk0709@gmail.com>
2018-11-30 09:18:13 -0800

    hola doodoo head

--- Unit1Lab/Unit6ALab.py
+++ Unit1Lab/Unit6ALab.py
@@ -13,55 +13,47 @@ def main():
 
 
 def textStuff():
+    WantToMakeInput = 'no'
     KeepGoing = 'yes'
+
+    textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Better Redistribute Bread','lol':'Lenin our leader', 'omg':'Opponents meet gulag','lmao':'Leninist-Marxist allied operation'}
+
     while KeepGoing == 'yes':
-        searchCode = input('what abbreviation you want')
-        textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Better Redistribute Bread','LOL':'Lenin our leader', 'OMG':'Opponents meet gulag','LMAO':'Leninist-Marxist allied operation'}
+        searchCode = input('what abbreviation you want, NO CAPS, all undercase')
 
 
         if searchCode in textCodes:
             print(textCodes[searchCode])
-        else:
-            WantToMakeInput = input('would u like to make new option')
 
-        if WantToMakeInput == 'yes':
-            print('ok')
-        elif WantToMakeInput == 'no':
-            print('thats too bad')
+        else:
+            print('sorry i cant find that')
+            WhatToDo = input('what do you want to do ')
+
+            if WhatToDo == 'delete':
+                delin = input('what abbreviation')
+                del textCodes[delin]
+            elif WhatToDo == 'change':
+                word = input('what is the abbreviation no capitols')
+                term = input('what is the saying')
+                textCodes[word] = term
+                #textCodes.update({word : term})
+                print('geez i didnt think of that one')
+
+
+
+
+            # WantToMakeInput = input('would u like to make new option')
+            # if WantToMakeInput == 'yes':
+
+
+            elif WantToMakeInput == 'no':
+                print('thats too bad')
+
+
+
+
 
         KeepGoing = input('wanna find another abreviation')
-
-
-
-
-
-
-    #
-    # searchCode = input('what abbreviation you want')
-    # textCodes = {'tyl':'text you later','gtg':'Get The Grenades','brb':'Better Redistribute Bread','LOL':'Lenin our leader', 'OMG':'Opponents meet gulag','LMAO':'Leninist-Marxist allied operation'}
-    #
-    #
-
-
-    #
-    # if searchCode in textCodes:
-    #      print(textCodes[searchCode])
-    # else:
-    #     WantToMakeInput = input('would u like to make new option')
-    #
-    # if WantToMakeInput == 'yes':
-    #     print('ok')
-    #
-
-
-
-
-
-
-
-
-
-
 
 
 
